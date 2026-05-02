82fe808b617a693d40b22ae13d22bc9e1e070efc
AleenaAfridi <aleenaafridi22@gmail.com>
2026-05-02 05:15:39 +0500

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,12 @@
+console.log("hello")
+
+const account_id = 12345
+let email = "abc@gmail.com"
+var password = "1111" /*prefer not to use var 
+                        because of block scope and functional scope*/
+city = "lahore"
+let account_state;
+
+//account_id= 234
+
+console.table([account_id, email, password, city, account_state])
